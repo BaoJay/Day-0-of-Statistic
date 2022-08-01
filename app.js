43ea53792c9a92f5d9aee0272c1c6768402716c2
Jay B <giabao.nguyenhoang1001@gmail.com>
2022-08-01 22:02:40 +0700

    Modify the satified input in Hackerrank

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function processData(input) {
   } else {
     // If 2n + 1
     // Find the median number index
-    medianIndex = (sortedArray.length - 1) / 2
+    medianIndex = (sortedArray.length - 1) / 2;
     // Find the median number in the middle of the array
     const medianNumber = sortedArray[medianIndex];
     console.log("This is the Median: ", medianNumber);
@@ -42,39 +42,55 @@ function processData(input) {
       }
     }
     return count;
-  }
+  };
 
   // Create an object of numbers with occurrences
   let numberWithOccurrences = {};
   for (let num of sortedArray) {
     numberWithOccurrences = {
       ...numberWithOccurrences,
-      [num]: countOccurrences(num)
-    }
+      [num]: countOccurrences(num),
+    };
   }
 
   const mostOccurItem = (object) => {
     // Turn the values of the object into an array
-    const sortedOccurArray = Object.values(object)
+    const sortedOccurArray = Object.values(object);
 
     // Turn the keys of the object into an array
-    const sortedNumberArray = Object.keys(object)
+    const sortedNumberArray = Object.keys(object);
 
-    // Find the index of the most occurrences 
-    const mostOccurIndex = sortedOccurArray.indexOf(Math.max(...sortedOccurArray))
+    // Find the index of the most occurrences
+    const mostOccurIndex = sortedOccurArray.indexOf(
+      Math.max(...sortedOccurArray)
+    );
 
     // Return the most occurrence number with that index
-    return sortedNumberArray[mostOccurIndex]
-  }
-  const result = mostOccurItem(numberWithOccurrences)
-  console.log("This is The Mode: ", result)
-  console.log("===============================================")
+    return sortedNumberArray[mostOccurIndex];
+  };
+  const result = mostOccurItem(numberWithOccurrences);
+  console.log("This is The Mode: ", result);
+  console.log("===============================================");
 }
 
-processData([1, 2, 3, 4, 5, 5]);
-processData([
-  64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060,
-]);
-processData([64041, 12415, 99233, 49781, 11743, 67060, 51135]);
+// processData([1, 2, 3, 4, 5, 5]);
+// processData([
+//   64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060,
+// ]);
+// processData([64041, 12415, 99233, 49781, 11743, 67060, 51135]);
+// processData([1, 2, 3, 4, 4, 5, 6]);
+// processData([13, 11, 22, 33, 33, 42, 49, 55, 53, 51, 611]);
 processData([1, 2, 3, 4, 4, 5, 6]);
-processData([13, 11, 22, 33, 33, 42, 49, 55, 53, 51, 611]);
+
+// console.log("10\n23 12 123 3 123 23 465 67 85 73");
+// processData("10\n23 12 123 3 123 23 465 67 85 73");
+// processData("23 12 123 3 123 23 465 67 85 73");
+
+// MODIFY INPUT IN THE HACKERRANK
+const inputX = "10\n23 12 123 3 123 23 465 67 85 73";
+// Choose numbers which is not preceded by \n
+const regexpNumber = /\b\d+\b(?!\n)/g;
+console.log("X by regexp: ", inputX.match(regexpNumber));
+const stringInput = inputX.match(regexpNumber);
+const numberInput = stringInput.map((num) => +num);
+console.log(numberInput);
