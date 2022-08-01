d72dd84b22978c3599f162a999edc54b95df4f7b
Nguyen Hoang Gia Bao <giabao@evol.vn>
2022-08-01 10:54:27 +0700

    done for The Mode logic

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 function processData(input) {
   //Enter your code here
 
-  // The Mean
+  // ============================ The Mean
   const x = [...input];
-  console.log("Array input", x);
-  const sumX = x.reduce((a, b) => a + b);
+  const sortedArray = x.sort((a, b) => a - b);
+  console.log("Array sorted input", sortedArray);
+  // console.log(sortedArray);
+  const sumX = sortedArray.reduce((a, b) => a + b);
   //   console.log(sumX);
   const theMean = sumX / x.length;
   console.log("This is the Mean: ", theMean);
 
-  // The Median
+  // ============================ The Median
   let medianIndex = 0;
   // If 2n
   if (x.length % 2 === 0) {
     // sort the array ascendant: tăng dần
-    const sortedArray = x.sort((a, b) => a - b);
-    // console.log(sortedArray);
+    
     medianIndex = sortedArray.length / 2;
     const medianNumber =
       (sortedArray[medianIndex] + sortedArray[medianIndex - 1]) / 2;
@@ -23,20 +24,52 @@ function processData(input) {
   } else {
     // If 2n + 1
     // sort the array ascendant: tăng dần
-    const sortedArray = x.sort((a, b) => a - b);
-    // console.log(sortedArray);
+
     medianIndex = (sortedArray.length - 1) / 2;
     const medianNumber = sortedArray[medianIndex];
     console.log("This is the Median: ", medianNumber);
   }
 
-  // The Mode
-  let sortedOccurrences = [];
-  for (let i = 0; i < sortedArray.length; i++) {
-    if (!sortedOccurrences[i]) {
-      sortedOccurrences.push(i);
+  // ============================ The Mode
+  
+  
+  const countOccurrences = (x) => {
+    let count = 0;
+    for ( let num of sortedArray ) {
+      if(num === x) {
+        count++;
+      }
+    }
+    return count;
+  }
+  
+  let numberWithOccurrences = {};
+  for (let num of sortedArray) {
+    numberWithOccurrences = {
+      ...numberWithOccurrences,
+      [num]: countOccurrences(num)
     }
   }
+  
+  
+  // Convert into array
+  
+  
+  const mostOccurItem = (object) => {
+    const sortedOccurArray = Object.values(object)
+    // console.log("Max number is: ",Math.max(...sortedOccurArray))
+    
+    const sortedNumberArray = Object.keys(object)
+    // console.log("sorted Occur Array: ", sortedOccurArray)
+    // console.log("sorted Number Array:",sortedNumberArray)
+    const mostOccurIndex = sortedOccurArray.indexOf(Math.max(...sortedOccurArray))
+
+    return sortedNumberArray[mostOccurIndex]
+  }
+  const result = mostOccurItem(numberWithOccurrences)
+  console.log("This is The Mode: ",result)
+
+  console.log("===============================================")
 }
 
 // processData([1, 2, 3, 4, 5, 5]);
@@ -44,3 +77,5 @@ function processData(input) {
 //   64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060,
 // ]);
 processData([64041, 12415, 99233, 49781, 11743, 67060, 51135]);
+processData([1, 2, 3, 4, 4, 5, 6]);
+processData([13, 11, 22, 33, 33, 42, 49, 55, 53, 51, 611]);
